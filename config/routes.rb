2e970f60f8b5f8a5281840b422285ac60f98e93b
Austin <admin@designxdevelop.com>
2016-05-11 17:59:34 -0700

    Generated Welcome_Controller
    Added Welcome Controller as placeholder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#index' # use to be get instead of root
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
