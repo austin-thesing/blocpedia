6b5ec810583d08a3be8dbf85a8be6f43d4e0be69
Austin Thesing <admin@designxdevelop.com>
2016-06-02 14:26:40 -0700

    Attempted Fix for Scope issues

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -24,7 +24,7 @@ class WikiPolicy < ApplicationPolicy
        elsif user.role == 'premium'
          all_wikis = scope.all
          all_wikis.each do |wiki|
-           if wiki.private == 'false' || wiki.owner == user || wiki.users.include?(user)
+           if wiki.private == 'true' || wiki.user == user || wiki.users.include?(user)
              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
            end
          end
@@ -32,7 +32,7 @@ class WikiPolicy < ApplicationPolicy
          all_wikis = scope.all
          wikis = []
          all_wikis.each do |wiki|
-           if wiki.private == 'false' || wiki.users.include?(user) #|| wiki.private == 'nil'
+           if (wiki.private == 'false' || wiki.private = 'nil') || wiki.users.include?(user)
              wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
            end
          end
