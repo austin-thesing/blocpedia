506acb721853ba89c75bc9187b8bcb7c3f8b8537
Austin Thesing <admin@designxdevelop.com>
2016-06-05 16:39:22 -0700

    Fixed Devise Custom Param Sanitization
    Added before_action callback on the devise_parameter_sanitizer
    
    Custom parameters now display in app again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  # before action callback for devise controller to utilize configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :firstname, :lastname, :username) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :firstname, :lastname, :username) }
+    #devise_parameter_sanitizer.for is depreciated in Devise 4.2 so I switched to .permit
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :firstname, :lastname, :username) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :firstname, :lastname, :username) }
   end
 end
